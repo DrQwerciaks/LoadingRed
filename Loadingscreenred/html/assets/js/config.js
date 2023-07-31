768e8d15d9b6b5dc2ad7d22a898595bfc3e355b8
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-07-31 23:09:28 +0200

    Add files via upload

---
+++ Loadingscreenred/html/assets/js/config.js
@@ -0,0 +1,17 @@
+const Config = {
+    links: {
+        discord: "Qwerciak Topka",
+        teamspeak: "QwerciakDEV",
+        tebex: "https://drqwerciak.pl/",
+    },
+    staff: [
+        {
+            role: "Owner",
+            name: "Qwerciak"
+        },
+        {
+            role: "Owner",
+            name: "RAguisedhsh"
+        },
+    ]
+}
